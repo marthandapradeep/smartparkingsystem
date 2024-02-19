16b7d39519dcb423495d9c169be110133c9f6698
Marthanda Pradeep Kurmala <marthandapradeepkurmala@gmail.com>
2024-02-19 19:12:22 +0530

    first commit

---
+++ vpms/admin/includes/footer.php
@@ -0,0 +1,10 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    
+                    <div class="col-sm-6 text-center">
+                        HYDERABAD'S NEXT GALLERIA Vehicle Parking Management System_2022@DEPT_OF_IT
+                    </div>
+                </div>
+            </div>
+        </footer>
